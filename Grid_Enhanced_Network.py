f0b1baefb58554476e304eb8029f30b6b872212c
AnastasisKratsios <anastasis.kratsios@math.ethz.ch>
2021-03-31 17:28:40 +0200

    h

--- Grid_Enhanced_Network.py
+++ Grid_Enhanced_Network.py
@@ -42,8 +42,8 @@ if trial_run == True:
                        'output_dim':[1]}
 
     param_grid_Deep_Classifier = {'batch_size': [32],
-                        'epochs': [600],
-                        'learning_rate': [0.0001],
+                        'epochs': [100],
+                        'learning_rate': [0.01],
                         'height': [400],
                         'depth': [2],
                         'input_dim':[15],

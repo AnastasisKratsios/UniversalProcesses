18d2b84a9851f891325053727bc2369e7bb501f7
AnastasisKratsios <anastasis.kratsios@math.ethz.ch>
2021-04-22 11:33:31 +0200

    working well

--- CV_Grid.py
+++ CV_Grid.py
@@ -25,7 +25,7 @@ if trial_run == True:
 
     param_grid_Deep_Classifier = {'batch_size': [32],
                         'epochs': [200],
-                        'learning_rate': [0.00001],
+                        'learning_rate': [0.001],
                         'height': [200],
                         'depth': [2],
                         'input_dim':[15],

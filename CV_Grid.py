c8d410b8f5faa3dd49a6629af12915e3a69c78ee
AnastasisKratsios <anastasis.kratsios@math.ethz.ch>
2021-04-27 16:38:45 +0200

    pushed some baby updates

--- CV_Grid.py
+++ CV_Grid.py
@@ -17,8 +17,8 @@ if trial_run == True:
     # Number of Jobs (Cores to use)
     n_jobs = 4
     # Number of Random CV Draws
-    n_iter = 1
-    n_iter_trees = 1#20
+    n_iter = 2
+    n_iter_trees = 2
     # Number of CV Folds
     CV_folds = 2
     # Number of Boostrapped Confidence Intervals
@@ -27,7 +27,7 @@ if trial_run == True:
 
     param_grid_Deep_Classifier = {'batch_size': [32],
                         'epochs': [50],
-                        'learning_rate': [0.0001],
+                        'learning_rate': [0.00001],
                         'height': [20],
                         'depth': [1],
                         'input_dim':[15],
@@ -62,20 +62,20 @@ else:
     # Training Parameters
     #----------------------#
     # Number of Jobs (Cores to use)
-    n_jobs = 4
+    n_jobs = 50
     # Number of Random CV Draws
-    n_iter = 5
-    n_iter_trees = 1
+    n_iter = 20
+    n_iter_trees = 20
     # Number of CV Folds
     CV_folds = 4
     # Number of Boostrapped Confidence Intervals
     N_Boostraps_BCA = 10**3
     
 
-    param_grid_Deep_Classifier = {'batch_size': [32],
-                        'epochs': [100,200,250],
-                        'learning_rate': [0.00001,0.000001],
-                        'height': [50,100,200,250],
+    param_grid_Deep_Classifier = {'batch_size': [8,16,32],
+                        'epochs': [100,200,250,400,600,700],
+                        'learning_rate': [0.0001,0.00001,0.000001,0.000001],
+                        'height': [50,100,200,250,400],
                         'depth': [2,3,4],
                         'input_dim':[15],
                         'output_dim':[1]}
